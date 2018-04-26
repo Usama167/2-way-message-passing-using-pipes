a8a864e185bba3772fa63d10ee9343551e3218ab
Usama167 <38745959+Usama167@users.noreply.github.com>
2018-04-26 10:37:12 +0500

    4,1.c

---
+++ 4,1.c
@@ -0,0 +1,75 @@
+//write first then read
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+ 
+int main()
+{
+    int fd;
+ 
+    // FIFO file path
+    char * myfifo2 = "/tmp/myfifo2";
+
+    // Creating the FIFO file 
+    mkfifo(myfifo2, 0666);
+    int login;
+    printf("Enter User to write from: ");
+    scanf("%d",&login);
+    //clearing buffer 
+    while ((getchar()) != '\n');
+    char arr1[80], arr2[80];
+    if(login==1){
+    while (1)
+    {
+	if((arr2[0]=='b' && arr2[1]=='y' && arr2[2]=='e')||(arr1[0]=='b' && arr1[1]=='y' && arr1[2]=='e')){
+		return 0;
+	}
+        // Open FIFO for write only
+        fd = open(myfifo2, O_WRONLY);
+ 
+	//user to write message
+        fgets(arr2, 80, stdin);
+	//write the message on FIFO file and close
+        write(fd, arr2, strlen(arr2)+1);
+        close(fd);
+ 
+        // Open FIFO for Read only
+        fd = open(myfifo2, O_RDONLY);
+ 
+        // Read from FIFO
+        read(fd, arr1, sizeof(arr1));
+ 
+        // Print the read message
+        printf("User4: %s\n", arr1);
+        close(fd);
+    }
+}
+	else if(login==4){
+	while(1){
+	if((arr2[0]=='b' && arr2[1]=='y' && arr2[2]=='e')||(arr1[0]=='b' && arr1[1]=='y' && arr1[2]=='e')){
+		return 0;
+	}
+	// Open FIFO for write only
+        fd = open(myfifo2,O_WRONLY);
+	
+	//user to write message
+        fgets(arr2, 80, stdin);
+	//write the message on FIFO file and close
+        write(fd, arr2, strlen(arr2)+1);
+        close(fd);
+	
+	// Open FIFO for Read only
+        fd = open(myfifo2,O_RDONLY);
+	// Read from FIFO	
+        read(fd, arr1, 80);
+	
+	// Print the read message	
+        printf("User1: %s\n", arr1);
+        close(fd);
+ }
+}
+    return 0;
+}
